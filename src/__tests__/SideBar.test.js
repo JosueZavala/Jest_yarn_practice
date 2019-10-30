689307f62e224a46b9228317f0e203c03ddc46c5
josuez <josue.zavala123@gmail.com>
2019-10-30 03:05:34 -0600

    Trying to up coverage of SideBar (failed)

--- src/__tests__/SideBar.test.js
+++ src/__tests__/SideBar.test.js
@@ -2,9 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import SideBar from '../components/App';
 import renderer from 'react-test-renderer';
+import {shallow, Enzyme, configure} from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+
+configure({ adapter: new Adapter() });
 
 test('Toggle Changed', () => {
-  const component = renderer.create(
+  /*const component = renderer.create(
     <SideBar />,
   );
   let tree = component.toJSON();
@@ -14,6 +18,8 @@ test('Toggle Changed', () => {
   tree.onClick();
   // re-rendering
   tree = component.toJSON();
-  expect(tree).toMatchSnapshot();
+  expect(tree).toMatchSnapshot();*/
 
+  const wrapper = shallow(<SideBar />);
+  wrapper.find('#menu-icon').simulate('click');
 });
